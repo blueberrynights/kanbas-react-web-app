73be963cf70df73d014e1e73f2ea089dd81768a0
blueberrynights <yang.ann@northeastern.edu>
2023-10-26 14:39:18 -0400

    lab

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,5 +1,13 @@
 import PassingFunctions from "./PassingFunctions";
 import ReduxExamples from "./ReduxExamples";
+import Add from "./Add";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariables";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
 
 function Assignment4() {
   function sayHello() {
@@ -8,8 +16,19 @@ function Assignment4() {
   return (
     <div>
       <h1>Assignment 4</h1>
-      <ReduxExamples/>
+      <Add a={1} b={2} />
       <PassingFunctions theFunction={sayHello} />
+      <EventObject></EventObject>
+      <Counter></Counter>
+      <BooleanStateVariables></BooleanStateVariables>
+      <StringStateVariables></StringStateVariables>
+      <DateStateVariable></DateStateVariable>
+      <ObjectStateVariable></ObjectStateVariable>
+      <ArrayStateVariable></ArrayStateVariable>
+
+
+
+      <ReduxExamples/>
       ...
     </div>
   );
