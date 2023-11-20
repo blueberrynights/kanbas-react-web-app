51d8366fa448112b3d8582b736b19525851eabda
Anna Yang <71159431+blueberrynights@users.noreply.github.com>
2023-11-20 00:44:15 -0500

    A5 (#5)
    * lab 5
    
    * a5
    
    * a5

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -3,13 +3,32 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import db from "./Database";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import axios from "axios";
 
 
 function Kanbas() {
   const [courses, setCourses] = useState(db.courses);
+  const URL = "http://localhost:4000/api/courses";
+  const addCourse = async () => {
+    const response = await axios.post(URL, course);
+    setCourses([
+      ...response.data,
+      ...courses,
+    ]);
+    setCourse({ name: "" });
+  };
+
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
+  };
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+
   const [course, setCourse] = useState({
     name: "New Course",      number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
@@ -17,20 +36,30 @@ function Kanbas() {
   const addNewCourse = () => {
     setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
   };
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
+  const deleteCourse = async (course) => {
+    const response = await axios.delete(
+      `${URL}/${course._id}`
+    );
+    setCourses(courses.filter(
+      (c) => c._id !== course._id));
   };
-  const updateCourse = () => {
+
+  const updateCourse = async (course) => {
+    const response = await axios.put(
+      `${URL}/${course._id}`,
+      course
+    );
     setCourses(
       courses.map((c) => {
         if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
+          return response.data;
         }
+        return c;
       })
     );
+    setCourse({ name: "" });
   };
+
 
   return (
     <Provider store={store}>
@@ -49,7 +78,7 @@ function Kanbas() {
                 deleteCourse={deleteCourse}
                 updateCourse={updateCourse}/>
             } />
-            <Route path="Courses/:courseId/*" element={
+            <Route path="courses/:courseId/*" element={
               <Courses courses={courses} />} />
 
 
